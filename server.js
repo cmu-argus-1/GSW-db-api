d5441c54d2f4d010d672eaaf986c70d57a1f73ec
tomyyyD <thomas5d@comcast.net>
2024-06-26 17:31:03 -0400

    starting to pull from mysql db

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { get_table } from './lib/api.js';
 import { host_ip } from './lib/secrets.js';
 
 
@@ -6,21 +7,20 @@ const app = express();
 const hostname = host_ip;
 const port = 3000;
 
-// const server = createServer(
-//     (req, res) => {
-//         const url = req.url;
-//         const method = req.method;
-//         if (method == 'GET' && url == '/'){
-//             res.statusCode = 200;
-//             res.setHeader('Content-Type', 'text/plain');
-//             res.end("Hello, World!");
-//         }
-//     }
-// )
-
 app.get('/', (req, res) => {
     res.send('Hello, world!');
 });
+app.get('/table/:id', (req, res) => {
+    get_table(req.params.id, (err, data) => {
+        if (err) {
+            console.log(err)
+            res.send("not found", 404)
+        } else {
+            res.send(JSON.stringify(data), 200)
+        }
+
+    })
+})
 
 app.listen(port, hostname, () => {
     console.log(`server running at http://${hostname}:${port}/`);
